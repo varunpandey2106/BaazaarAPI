af72f4da39d55d1bb3c2f5efcf56f2883253d542
varunpandey2106 <varun.pandey2106@gmail.com>
2023-09-21 23:13:14 +0530

    product serializer updated .

--- products/serializers.py
+++ products/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Category, Product
+from drf_extra_fields.fields import Base64ImageField
+
 
 
 
@@ -25,4 +27,15 @@ class CreateProductSerializer(serializers.ModelSerializer):
         model = Product
         exclude = ("modified",)
         # read_only_fields = ('id', 'seller', 'category', 'title', 'price', 'image', 'description', 'quantity', 'views',)
-        
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+    seller = serializers.SlugRelatedField(slug_field="username", queryset=User.objects)
+    category = serializers.SerializerMethodField()
+    image = Base64ImageField()
+
+    def get_category(self, obj):
+        return obj.category.name
+
+    class Meta:
+        model = Product
+        exclude = "modified"
