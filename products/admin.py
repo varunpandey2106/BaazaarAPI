93066cf375d0a5eec163d9145b4a78fe5c9ad7f0
varunpandey2106 <varun.pandey2106@gmail.com>
2023-09-21 22:56:38 +0530

    serializers added

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product, ProductImage,ProductSpecification, ProductSpecificationValue, ProductType, Category
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductType)
+admin.site.register(ProductSpecification)
+admin.site.register(ProductSpecificationValue)
+admin.site.register(ProductImage)
