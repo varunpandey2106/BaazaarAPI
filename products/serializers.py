83619f92860cb60d194d25740eb3515b8cf638e4
varunpandey2106 <varun.pandey2106@gmail.com>
2023-09-21 23:03:15 +0530

    product serializer added

--- products/serializers.py
+++ products/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
-from .models import Category
+from django.contrib.auth.models import User
+from .models import Category, Product
 
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
+        exclude = "modified"
+
+class ProductSerializer(serializers.ModelSerializer):
+    seller = serializers.SlugRelatedField(slug_field="username", queryset=User.objects)
+    category = serializers.SerializerMethodField()
+
+    def get_category(self, obj): #custom method, determines value of category of each serialized Product instance 
+        return obj.category.name #retrieves name of catgeory and returns val
+
+    class Meta:
+        model = Product
         exclude = "modified"
