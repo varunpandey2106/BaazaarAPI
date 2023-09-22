de9e5fe9c3ac2354c886c6d64f2fbe8e8bd16cd1
varunpandey2106 <varun.pandey2106@gmail.com>
2023-09-22 13:05:30 +0530

    haystack added

--- products/serializers.py
+++ products/serializers.py
@@ -83,16 +83,17 @@ class ProductDocumentSerializer(DocumentSerializer):
         document = ProductDocument
         exclude = "modified"
 
-# class ProductIndexSerializer(HaystackSerializer):
-#     class Meta:
-#         # The `index_classes` attribute is a list of which search indexes
-#         # we want to include in the search.
-#         index_classes = [ProductIndex]
+class ProductIndexSerializer(HaystackSerializer):
+    class Meta:
+        # The `index_classes` attribute is a list of which search indexes
+        # we want to include in the search.
+        index_classes = [ProductIndex]
 
-#         # The `fields` contains all the fields we want to include.
-#         # NOTE: Make sure you don't confuse these with model attributes. These
-#         # fields belong to the search index!
-#         fields = ("text", "title", "category",)
+        # The `fields` contains all the fields we want to include.
+        # NOTE: Make sure you don't confuse these with model attributes. These
+        # fields belong to the search index!
+        fields = ("text", "title", "category",)
+
 
 
 
