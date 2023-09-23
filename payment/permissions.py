2dfb8c6b99de71f897a71ac9dce88af32d5db635
varunpandey2106 <varun.pandey2106@gmail.com>
2023-09-23 16:53:57 +0530

    paymentpending? permissions

--- payment/permissions.py
+++ payment/permissions.py
@@ -14,3 +14,15 @@ class IsPaymentByUser(BaseException):
 
     def has_object_permission(self, request, view, obj):
         return obj.order.buyer == request.user or request.user.is_staff
+    
+class IsPaymentPending(BasePermission):
+    """
+    Check if the status of payment is pending or completed before updating/deleting instance
+    """
+    message = _('Updating or deleting completed payment is not allowed.')
+
+    def has_object_permission(self, request, view, obj):
+        if view.action in ('retrieve',):
+            return True
+        return obj.status == 'P'
+
