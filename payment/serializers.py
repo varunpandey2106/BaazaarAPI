9b33bc94158b6b64f933eae62377b81535eecb16
varunpandey2106 <varun.pandey2106@gmail.com>
2023-09-23 16:19:40 +0530

    payment serializer b

--- payment/serializers.py
+++ payment/serializers.py
@@ -16,3 +16,17 @@ class PaymentSerializer(serializers.ModelSerializer):
         fields = ('id', 'buyer', 'status', 'payment_option',
                   'order', 'created_at', 'updated_at')
         read_only_fields = ('status', )
+
+class PaymentOptionSerializer(serializers.ModelSerializer):
+    """
+    Payment serializer for checkout. Order will be automatically set during checkout.
+    """
+    buyer = serializers.CharField(
+        source='order.buyer.get_full_name', read_only=True)
+
+    class Meta:
+        model = Payment
+        fields = ('id', 'buyer', 'status', 'payment_option',
+                  'order', 'created_at', 'updated_at')
+        read_only_fields = ('status', 'order')
+
