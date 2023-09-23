60fd9432abf6c6df47bf1eb05e2a79d789092c9c
varunpandey2106 <varun.pandey2106@gmail.com>
2023-09-23 16:56:23 +0530

    order permissions

--- payment/permissions.py
+++ payment/permissions.py
@@ -36,3 +36,15 @@ class IsPaymentForOrderNotCompleted(BasePermission):
             order = get_object_or_404(Order, id=order_id)
             return order.status != 'C'
         return False
+    
+class DoesOrderHaveAddress(BasePermission):
+    message = _(
+        'Creating a checkout session without having a shipping and billing address is not allowed.')
+
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            order_id = view.kwargs.get('order_id')
+            order = get_object_or_404(Order, id=order_id)
+            return order.shipping_address and order.billing_address
+        return False
+
