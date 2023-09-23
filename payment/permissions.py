cfaed695af3b9dce1d9d59d8c5e3876f27e73c27
varunpandey2106 <varun.pandey2106@gmail.com>
2023-09-23 16:55:29 +0530

    checkoutforcompletedpayment permissions

--- payment/permissions.py
+++ payment/permissions.py
@@ -26,3 +26,13 @@ class IsPaymentPending(BasePermission):
             return True
         return obj.status == 'P'
 
+class IsPaymentForOrderNotCompleted(BasePermission):
+    message = _(
+        'Creating a checkout session for completed payment is not allowed.')
+
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            order_id = view.kwargs.get('order_id')
+            order = get_object_or_404(Order, id=order_id)
+            return order.status != 'C'
+        return False
