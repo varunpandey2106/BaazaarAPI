365d5a1f55daa012ba6feb4555cfed871a2bc702
varunpandey2106 <varun.pandey2106@gmail.com>
2023-09-24 18:15:52 +0530

    cartmodel added

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
 
 # Create your models here.
+
+User= get_user_model()
+
+class Cart:
+    user = models.OneToOneField(
+        User, related_name="user_cart", on_delete=models.CASCADE
+    )
+    total = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0, blank=True, null=True
+    )
+
+
+class CartItem:
+    pass
