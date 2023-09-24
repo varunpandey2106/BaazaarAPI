0f5181629771cd6276b4b4336337daf922315b70
varunpandey2106 <varun.pandey2106@gmail.com>
2023-09-24 18:21:14 +0530

    cartitemmodel added

--- cart/models.py
+++ cart/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from products.models import Product
+
 
 
 # Create your models here.
@@ -21,5 +23,19 @@ def create_user_cart(sender, created, instance, *args, **kwargs):
     if created:
         Cart.objects.create(user=instance)
 
+
+class TimeStampedModel(models.Model):
+    created_at= models.DateTimeField(auto_now_add=True) #profile create time
+    updated_at= models.DateTimeField(auto_now_add=True) #profile update time
+
+    class Meta:
+        abstract= True
+
+
 class CartItem:
-    pass
+    class CartItem(TimeStampedModel):
+        cart = models.ForeignKey(Cart, related_name="cart_item", on_delete=models.CASCADE)
+        product = models.ForeignKey(
+        Product, related_name="cart_product", on_delete=models.CASCADE
+    )
+    quantity = models.IntegerField(default=1)
