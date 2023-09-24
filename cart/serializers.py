11df1b32779811a9e9498fbccc1e0a7ac93c3a20
varunpandey2106 <varun.pandey2106@gmail.com>
2023-09-24 18:34:56 +0530

    cart item update serializer added

--- cart/serializers.py
+++ cart/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Cart, CartItem
 from products.models import Product
 
-class CartProductSerializer(serializers.ModelSerializer):
+class CartProductSerializer(serializers.ModelSerializer): #serialize the Product model.
     class Meta:
         model = Product
         fields = (
@@ -13,15 +13,20 @@ class CartProductSerializer(serializers.ModelSerializer):
             "image",
         )
 
-class CartItemSerializer(serializers.ModelSerializer):
+class CartItemSerializer(serializers.ModelSerializer): #serialize the CartItem model.
     # product = CartProductSerializer(required=False)
     class Meta:
         model = CartItem
         fields = ["cart", "product", "quantity"]
 
-class CartItemMiniSerializer(serializers.ModelSerializer):
+class CartItemMiniSerializer(serializers.ModelSerializer): #serialize CartItem instances
     product = CartProductSerializer(required=False)
 
+    class Meta:
+        model = CartItem
+        fields = ["product", "quantity"]
+
+class CartItemUpdateSerializer(serializers.ModelSerializer): # updating CartItem instances.
     class Meta:
         model = CartItem
         fields = ["product", "quantity"]
