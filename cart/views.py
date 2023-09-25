f0296f674e12361c7093b1d527c5185276bc113a
varunpandey2106 <varun.pandey2106@gmail.com>
2023-09-25 22:43:36 +0530

    cart item api view added

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,57 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .serializers import CartItemSerializer
+from .models import Cart, CartItem
+from products.models import Product
+from rest_framework import permissions, status
+from django.utils.translation import gettext_lazy as _
+from rest_framework.exceptions import NotAcceptable, ValidationError, PermissionDenied
+from rest_framework.response import Response
+# from notifications.utils import push_notification
+
 
 # Create your views here.
 
 
+class CartItemAPIView(ListCreateAPIView):
+    serializer_class = CartItemSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = CartItem.objects.filter(cart__user=user)
+        return queryset
+
+    def create(self, request, *args, **kwargs):
+        user = request.user
+        cart = get_object_or_404(Cart, user=user)
+        product = get_object_or_404(Product, pk=request.data["product"])
+        current_item = CartItem.objects.filter(cart=cart, product=product)
+
+        if user == product.user:
+            raise PermissionDenied("This Is Your Product")
+
+        if current_item.count() > 0:
+            raise NotAcceptable("You already have this item in your shopping cart")
+
+        try:
+            quantity = int(request.data["quantity"])
+        except Exception as e:
+            raise ValidationError("Please Enter Your Quantity")
+
+        if quantity > product.quantity:
+            raise NotAcceptable("You order quantity more than the seller have")
+
+        cart_item = CartItem(cart=cart, product=product, quantity=quantity)
+        cart_item.save()
+        serializer = CartItemSerializer(cart_item)
+        total = float(product.price) * float(quantity)
+        cart.total = total
+        cart.save()
+        # push_notifications(
+        #     cart.user,
+        #     "New cart product",
+        #     "you added a product to your cart " + product.title,
+        # )
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
